5754b6248d03e05ee3ef73ce537e56695f14a07e
cjvanlissa <c.j.vanlissa@uu.nl>
2020-04-20 18:41:18 +0200

    Fix bug in handling duplicates (duplicate removal was contingent on region variable existing)

--- scripts/functions/merge_metadata.R
+++ scripts/functions/merge_metadata.R
@@ -30,22 +30,23 @@ merge_files <- function(df, which_file, metadata_location){
     if(!is.null(df_dat[["region"]])){
       message("Dropping region to see if this solves the problem.")
       df_dat <- df_dat[is.na(df_dat$region), ]
-      if(any(duplicated(df_dat$countryiso3))){
-        message("There are still duplicated country names in ", which_file, ". Using the row with most available data. Manual merging might be required. The duplicates are:\n  ", paste0(df_dat$countryiso3[which(duplicated(df_dat$countryiso3))], collapse = "\n  "))
-        the_dups <- which(duplicated(df_dat$countryiso3) | duplicated(df_dat$countryiso3, fromLast = TRUE))
-        df_dups <- df_dat[the_dups, ]
-        df_dat <- df_dat[-the_dups, ]
-        df_dups$miss <- rowSums(is.na(df_dups))
-        for(countr in unique(df_dups$countryiso3)){
-          df_dups <- df_dups[-which(df_dups$countryiso3 == countr)[-which.min(df_dups$miss[df_dups$countryiso3 == countr])], ]
-        }
-        df_dat <- rbind(df_dat, df_dups[, -ncol(df_dups)])
-        message("For duplicates, used row with most complete information.")
-        #df_dat <- df_dat[!duplicated(df_dat$countryiso3), ]
-      }
     }
-  }  
-  merge(df, df_dat, all.x = TRUE, by = "countryiso3")
+    if(any(duplicated(df_dat$countryiso3))){
+      message("There are still duplicated country names in ", which_file, ". Using the row with most available data. Manual merging might be required. The duplicates are:\n  ", paste0(df_dat$countryiso3[which(duplicated(df_dat$countryiso3))], collapse = "\n  "))
+      the_dups <- which(duplicated(df_dat$countryiso3) | duplicated(df_dat$countryiso3, fromLast = TRUE))
+      df_dups <- df_dat[the_dups, ]
+      df_dat <- df_dat[-the_dups, ]
+      df_dups$miss <- rowSums(is.na(df_dups))
+      for(countr in unique(df_dups$countryiso3)){
+        df_dups <- df_dups[-which(df_dups$countryiso3 == countr)[-which.min(df_dups$miss[df_dups$countryiso3 == countr])], ]
+      }
+      df_dat <- rbind(df_dat, df_dups[, -ncol(df_dups)])
+      message("For duplicates, used row with most complete information.")
+      #df_dat <- df_dat[!duplicated(df_dat$countryiso3), ]
+    }
+  }
+}  
+merge(df, df_dat, all.x = TRUE, by = "countryiso3")
 }
 
 get_metadata <- function(metadata_location, overwrite){
