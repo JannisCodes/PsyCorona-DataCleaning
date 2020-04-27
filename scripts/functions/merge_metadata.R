34d6e76874ba8e0a84ae38942bc7a61d2cefb954
Jannis Kreienkamp <38377896+JannisCodes@users.noreply.github.com>
2020-04-27 15:25:33 +0200

    Revert "Fix bug in merge_metadata"

--- scripts/functions/merge_metadata.R
+++ scripts/functions/merge_metadata.R
@@ -7,17 +7,12 @@ which_files <- function(metadata_location){
   gsub("^.+\\/(.+)\\.csv$", "\\1", list_data(metadata_location))
 }
 
-this_file <- function(which_file, metadata_location){
-  f <- list_data(metadata_location)
-  grep(paste0("\\b", tolower(which_file), ".csv"), tolower(f), value = TRUE)
-}
-
-
 merge_files <- function(df, which_file, metadata_location){
   if(is.null(df[["countryiso3"]])){
     df$countryiso3 <- countrycode::countrycode(df$coded_country, origin = "country.name", destination = "iso3c")
   }
   f <- list_data(metadata_location)
+  which_file = "CSSE"
   f <- grep(paste0("\\b", tolower(which_file), ".csv"), tolower(f), value = TRUE)
   if(length(f) > 1) stop("No unique match with filenames.")
   df_dat <- read.csv(f, stringsAsFactors = FALSE)
@@ -30,22 +25,20 @@ merge_files <- function(df, which_file, metadata_location){
     if(!is.null(df_dat[["region"]])){
       message("Dropping region to see if this solves the problem.")
       df_dat <- df_dat[is.na(df_dat$region), ]
-    }
-    if(any(duplicated(df_dat$countryiso3))){
-      message("There are still duplicated country names in ", which_file, ". Using the row with most available data. Manual merging might be required. The duplicates are:\n  ", paste0(df_dat$countryiso3[which(duplicated(df_dat$countryiso3))], collapse = "\n  "))
-      the_dups <- which(duplicated(df_dat$countryiso3) | duplicated(df_dat$countryiso3, fromLast = TRUE))
-      df_dups <- df_dat[the_dups, ]
-      df_dat <- df_dat[-the_dups, ]
-      df_dups$miss <- rowSums(is.na(df_dups))
-      for(countr in unique(df_dups$countryiso3)){
-        df_dups <- df_dups[-which(df_dups$countryiso3 == countr)[-which.min(df_dups$miss[df_dups$countryiso3 == countr])], ]
+      if(any(duplicated(df_dat$countryiso3))){
+        message("There are still duplicated country names in ", which_file, ". Using the row with most available data. Manual merging might be required. The duplicates are:\n  ", paste0(df_dat$countryiso3[which(duplicated(df_dat$countryiso3))], collapse = "\n  "))
+        the_dups <- which(duplicated(df_dat$countryiso3) | duplicated(df_dat$countryiso3, fromLast = TRUE))
+        df_dups <- df_dat[the_dups, ]
+        df_dups$miss <- rowSums(is.na(df_dups))
+        for(countr in unique(df_dups$countryiso3)){
+          df_dat <- df_dat[-the_dups[df_dups$countryiso3 == countr][-which.min(df_dups$miss[df_dups$countryiso3 == countr])], ]
+        }
+        message("For duplicates, used row with most complete information.")
+        #df_dat <- df_dat[!duplicated(df_dat$countryiso3), ]
       }
-      df_dat <- rbind(df_dat, df_dups[, -ncol(df_dups)])
-      message("For duplicates, used row with most complete information.")
-      #df_dat <- df_dat[!duplicated(df_dat$countryiso3), ]
     }
-  }
-merge(df, df_dat, all.x = TRUE, by = "countryiso3")
+  }  
+  merge(df, df_dat, all.x = TRUE, by = "countryiso3")
 }
 
 get_metadata <- function(metadata_location, overwrite){
